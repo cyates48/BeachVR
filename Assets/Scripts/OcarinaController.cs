7684767708966945b7d5e3e6eab7f435f4575205
cyates48 <cmyates48@gmail.com>
2018-10-26 11:41:59 -0700

    Added SunMelody and OcarinaController scripts

---
+++ Assets/Scripts/OcarinaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcarinaController : MonoBehaviour {
+
+    // LOTS OF LOGIC to go here. 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SunMelody.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMelody : MonoBehaviour {
+
+    private int[] sunMelodyNotes = {5, 7, 1, 3, 1, 7};
+    private bool isPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPlayed == true)
+        {
+            // play melody
+            
+        }
+		
+	}
+
+    void checkMelody ( params int[] controllerSequence) {
+        bool token = true;
+        for (int i = 0; i<6; i++) {
+            if (controllerSequence[i] != sunMelodyNotes[i]) {
+                token = false;
+                break;
+            }
+        }
+        isPlayed = token;
+    }   
+}
